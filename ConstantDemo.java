665cba511326e67a361ecceea93aac9a649f9fcd
JunhwanKim <67085700+EverythingForJ@users.noreply.github.com>
2020-07-20 13:51:44 +0900

    Add Constant
    20/07

---
+++ ConstantDemo.java
@@ -0,0 +1,15 @@
+/*
+작성자 : 
+작성목적 : 
+작성일지 : 20/07/2020
+작성환경 : OS(windows10), JAVA VERSION(JDK-14.0.2). , 어떤 에디터를 썻는지(eclips
+*/
+
+// System.out.println();
+public class ConstantDemo {
+	public static void main(String[] args) {
+		double pi = 3.141592; // 변수
+		System.out.printf("pi의 값은 %f입니다.", pi);
+
+	}
+}
