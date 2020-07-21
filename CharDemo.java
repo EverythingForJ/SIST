b0aa53e66cb084cbe26c104c63d5fbecb688cff3
JunhwanKim <67085700+EverythingForJ@users.noreply.github.com>
2020-07-21 10:28:54 +0900

    Add CharDemo

---
+++ CharDemo.java
@@ -0,0 +1,18 @@
+/*
+작성자 : 김준환
+작성목적 : CharDemo
+작성일지 : 20/07/2020
+작성환경 : Windows 10, OpenJDK-14.0.2, EditPlus 5.3
+*/
+
+public class CharDemo {
+	public static void main(String[] args) {
+		//System.out.println("오늘은 특별히 \"java의 실수저장법\"에 대해서 학습했습니다.");
+		//String filePath = "C:\\Program Files\\jdk";
+		//String filePath = "C:/Program Files/jdk";
+		//System.out.println(filePath);
+		char ch = '\uCAFE';
+//		System.out.printf("ch = %d\n", ch);
+		System.out.printf("ch = %c\n", ch);
+	}
+}
