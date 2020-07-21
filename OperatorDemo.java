746f0570c8847151cd5f170807cc074e84e5a8c8
JunhwanKim <67085700+EverythingForJ@users.noreply.github.com>
2020-07-21 16:51:59 +0900

    Add OperatorDemo

---
+++ OperatorDemo.java
@@ -0,0 +1,15 @@
+/*
+작성자 : 김준환
+작성목적 : 연산자
+작성일지 : 21/07/2020
+작성환경 : Windows 10, OpenJDK-14.0.2, EditPlus 5.3
+*/
+
+public class OperatorDemo {
+	public static void main(String[] args) {
+		// .(~의, ~에 속해있는) [] ()
+		System.out.println("4+5*6 ="+ (4+5*6)); // () 연산자
+		String[] fruits = {"사과","배","딸기","복숭아"};
+			System.out.println(fruits[2]);
+	}
+}
