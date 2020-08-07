7121b002f3c6ecc839818be53af13e3931423eb6
JunhwanKim <67085700+EverythingForJ@users.noreply.github.com>
2020-08-07 14:06:21 +0900

    Add Instanceof
    07/08

---
+++ Polymorphism/InstanceofDemo.java
@@ -0,0 +1,18 @@
+
+
+public class InstanceofDemo {
+
+	public static void main(String[] args) {
+		Mammal m = new Mammal();//부모형
+		Korean ko = new Korean();//자식형
+		// 한국인은 포유류이다. -> Korean is a Mammal.
+		// m=ko; // 자식이 부모형으로 변환하는 것은 무조건 성공한다.
+		// 포유류는 한국인이다???? X
+		// ko = (Korean)m; //자동형변환에러, 강제형변환 컴파일 에러
+		Object obj = new Object();
+		String str = new String();
+		obj = str; // 만약 전에 형변환이 한번되었었다면 밑에 라인 에러없이 실행가능
+		str = (String)obj;
+	}
+
+}
