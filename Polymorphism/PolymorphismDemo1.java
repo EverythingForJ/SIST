96da7e2bb7106d9cadbd142d9bb805101ef53dc9
JunhwanKim <67085700+EverythingForJ@users.noreply.github.com>
2020-08-07 15:31:03 +0900

    Add PolymorphismDemo
    07/08

---
+++ Polymorphism/PolymorphismDemo1.java
@@ -0,0 +1,24 @@
+
+
+public class PolymorphismDemo1 {
+	public static void main(String[] args) {
+//		Sonata[] array = new Sonata[2];
+//		array[0] = new Sonata("Black Sonata");
+//		array[1] = new Matiz("Silver Sonata"); // error
+		
+		// Heterogeneous Collection //다른 종류의 배열
+//		Car[] array = new Car[3];
+//		array[0] = new Sonata("Black Sonata");
+//		array[1] = new Matiz("Silver Sonata");
+//		array[2] = new Carnival("White Carnival");
+		
+//		Car[] array = {new Sonata("Black Sonata"), new Matiz("Silver Sonata"), new Carnival("White Carnival")};
+//		for(Car car:array ) System.out.println(car);
+		
+//		Object[] array = new Object[3]; // object는 모든게 다들어가서 이 배열에 뭐가들었는지 전혀 알 수 없어서 문제점이 발생한다.
+//		array[0] = new Mammal();
+//		array[1] = new Sonata("sa");
+//		array[2] = new String("asdas");
+		
+	}
+}
