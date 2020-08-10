4b813f0ddaa5a1cee526481e17c6211aa1c4cf70
JunhwanKim <67085700+EverythingForJ@users.noreply.github.com>
2020-08-10 17:17:43 +0900

    Add Abstract
    10/08

---
+++ Abstract/AbstractDemo.java
@@ -0,0 +1,22 @@
+
+
+public class AbstractDemo {
+	public static void main(String[] args) {
+		// Mammal m = new Mammal();
+		//Mammal m;
+		//m = new Lion();
+		//Mammal m1 = new Tiger();
+		
+//		Mammal m = new Mammal();
+//		Lion lion = new Lion();
+//		m = lion;
+//		System.out.println(lion.name);
+		
+//		Mammal lion = new Lion();
+//		System.out.println(lion.name);
+		
+		Mammal lion = new Lion("Sally");
+		lion.print();
+		
+	}
+}
---
+++ Abstract/AbstractDemo1.java
@@ -0,0 +1,10 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class AbstractDemo1 {
+	public static void main(String[] args) {
+		// Calendar now = new Calendar(); // 캘린더는 추상 클래스라 인스턴스화 불가
+		Calendar now = new GregorianCalendar();
+		
+	}
+}
