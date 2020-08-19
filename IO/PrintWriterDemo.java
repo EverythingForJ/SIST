8ea4cad52359bb340b887522aed40108c616164e
JunhwanKim <67085700+EverythingForJ@users.noreply.github.com>
2020-08-20 08:55:13 +0900

    Add PrintWriter
    20/08

---
+++ IO/PrintWriterDemo.java
@@ -0,0 +1,22 @@
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class PrintWriterDemo {
+	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
+		try(PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:/temp/today"))), true)){
+			String line = null;
+			int count = 0;
+			while((line = scan.nextLine()) != null) {
+				pw.printf("%-3d : %s%n", ++count, line);
+			}
+			System.out.println("File saved successfully.");
+		}catch(IOException e) {
+			System.out.println(e);
+		}
+	}
+}
